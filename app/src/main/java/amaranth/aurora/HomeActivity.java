1659d49de946a0f0c9b14a59a26fdde93c146c9c
HassanSyedt <hmobeen@umass.edu>
2015-03-31 19:22:27 -0400

    Changed HomeActivity to type Activty to get rid of the annoying actionbar that would pop up whenever it was being used.

--- app/src/main/java/amaranth/aurora/HomeActivity.java
+++ app/src/main/java/amaranth/aurora/HomeActivity.java
@@ -1,12 +1,13 @@
 package amaranth.aurora;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,7 @@ public class HomeActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+       // getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
@@ -27,13 +28,14 @@ public class HomeActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        /*
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
+*/
         return super.onOptionsItemSelected(item);
     }
 }
