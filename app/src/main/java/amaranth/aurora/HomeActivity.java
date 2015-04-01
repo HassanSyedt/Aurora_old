c1c1f712c4c6e9c622ef6ef15beb913f25b92be3
HassanSyedt <hmobeen@umass.edu>
2015-03-31 20:11:25 -0400

    Added menu items. Need to give them actions. Gave icons. Changed homeactivity back to an actionbar activity

--- app/src/main/java/amaranth/aurora/HomeActivity.java
+++ app/src/main/java/amaranth/aurora/HomeActivity.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,7 @@ public class HomeActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-       // getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
@@ -28,14 +28,14 @@ public class HomeActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        /*
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-*/
+
         return super.onOptionsItemSelected(item);
     }
 }
