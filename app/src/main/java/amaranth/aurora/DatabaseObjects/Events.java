ae5f3c45d125a60133b4e7fed2258086591dac69
mavsfan <marcp7611@gmail.com>
2015-04-12 01:32:27 -0400

    Added classes for database

---
+++ app/src/main/java/amaranth/aurora/DatabaseObjects/Events.java
@@ -0,0 +1,14 @@
+package amaranth.aurora.DatabaseObjects;
+
+/**
+ * Created by marc on 4/12/2015.
+ */
+public class Events {
+    public int eid;      //event id
+    public int ownerid;  // owner id
+    public String ename; //event name
+    public String etype; //public vs private
+    public String ecat;  //public category
+    public String eDscp; //description
+    public String eaddress; //address
+}
